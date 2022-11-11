93fd31ed16c6c5894c2a15667a87fb7496eeef01
nefidesarrollocesde <nefidesarrollocesde@gmail.com>
2022-11-11 06:49:49 -0500

    se crea proyecto

---
+++ API/ServidorAPI.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import { rutasPersonalisadas } from '../Routes/rutas.js';
+import { conectarConMongo } from '../Database/conexion.js';
+import cors from 'cors'
+
+export class ServidorAPI{
+    constructor(){
+        this.app = express()
+        this.conectarConBD()
+        this.activarBody()
+        this.atenderPeticiones()
+    }
+
+    //METODOS DE LA CLASE ServidorAPI
+    despertarServidor(){
+        this.app.listen(process.env.PORT, function(){
+            console.log("Exito servico activo: " + process.env.PORT);
+        })
+    }
+
+    atenderPeticiones(){
+        this.app.use('/', rutasPersonalisadas)
+    }
+
+    conectarConBD(){
+        conectarConMongo()
+    }
+
+    activarBody(){
+        this.app.use(cors())
+        this.app.use(express.json())
+    }
+}
